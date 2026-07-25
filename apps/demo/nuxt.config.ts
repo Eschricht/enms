0aa6e7b641e78b10e8cf6aa434257254365665a3
Johannes <johannes.eschricht@gmail.com>
2026-07-25 15:04:14 +0200

    chore: bump packages

--- apps/demo/nuxt.config.ts
+++ apps/demo/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 5,
+  },
+  experimental: { nitroAutoImports: true },
+  compatibilityDate: '2026-06-28',
   extends: ['@enms/layer-a'],
   modules: [
     '@nuxt/ui',
